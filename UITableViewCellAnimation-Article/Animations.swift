7e515912865a94548520f3db180ddeecf43bcd98
Adam <ad-walker@users.noreply.github.com>
2020-03-23 15:14:50 -0400

    Refactor to use base classes UIView and UIScrollView
    Animations can be apply to derived classes such as UICollectionView, etc.

--- UITableViewCellAnimation-Article/Animations.swift
+++ UITableViewCellAnimation-Article/Animations.swift
@@ -8,66 +8,66 @@
 
 import UIKit
 
-typealias Animation = (UITableViewCell, IndexPath, UITableView) -> Void
+typealias Animation = (UIView, IndexPath, UIScrollView) -> Void
 
 enum AnimationFactory {
 
-	static func makeFade(duration: TimeInterval, delayFactor: Double) -> Animation {
-		return { cell, indexPath, _ in
-			cell.alpha = 0
+    static func makeFade(duration: TimeInterval, delayFactor: Double) -> Animation {
+        return { cell, indexPath, _ in
+            cell.alpha = 0
 
-			UIView.animate(
-				withDuration: duration,
-				delay: delayFactor * Double(indexPath.row),
-				animations: {
-					cell.alpha = 1
-			})
-		}
-	}
+            UIView.animate(
+                withDuration: duration,
+                delay: delayFactor * Double(indexPath.row),
+                animations: {
+                    cell.alpha = 1
+            })
+        }
+    }
 
-	static func makeMoveUpWithBounce(rowHeight: CGFloat, duration: TimeInterval, delayFactor: Double) -> Animation {
-		return { cell, indexPath, tableView in
-			cell.transform = CGAffineTransform(translationX: 0, y: rowHeight)
+    static func makeMoveUpWithBounce(rowHeight: CGFloat, duration: TimeInterval, delayFactor: Double) -> Animation {
+        return { cell, indexPath, scrollView in
+            cell.transform = CGAffineTransform(translationX: 0, y: rowHeight)
 
-			UIView.animate(
-				withDuration: duration,
-				delay: delayFactor * Double(indexPath.row),
-				usingSpringWithDamping: 0.4,
-				initialSpringVelocity: 0.1,
-				options: [.curveEaseInOut],
-				animations: {
-					cell.transform = CGAffineTransform(translationX: 0, y: 0)
-			})
-		}
-	}
+            UIView.animate(
+                withDuration: duration,
+                delay: delayFactor * Double(indexPath.row),
+                usingSpringWithDamping: 0.4,
+                initialSpringVelocity: 0.1,
+                options: [.curveEaseInOut],
+                animations: {
+                    cell.transform = CGAffineTransform(translationX: 0, y: 0)
+            })
+        }
+    }
 
-	static func makeSlideIn(duration: TimeInterval, delayFactor: Double) -> Animation {
-		return { cell, indexPath, tableView in
-			cell.transform = CGAffineTransform(translationX: tableView.bounds.width, y: 0)
+    static func makeSlideIn(duration: TimeInterval, delayFactor: Double) -> Animation {
+        return { cell, indexPath, scrollView in
+            cell.transform = CGAffineTransform(translationX: scrollView.bounds.width, y: 0)
 
-			UIView.animate(
-				withDuration: duration,
-				delay: delayFactor * Double(indexPath.row),
-				options: [.curveEaseInOut],
-				animations: {
-					cell.transform = CGAffineTransform(translationX: 0, y: 0)
-			})
-		}
-	}
+            UIView.animate(
+                withDuration: duration,
+                delay: delayFactor * Double(indexPath.row),
+                options: [.curveEaseInOut],
+                animations: {
+                    cell.transform = CGAffineTransform(translationX: 0, y: 0)
+            })
+        }
+    }
 
-	static func makeMoveUpWithFade(rowHeight: CGFloat, duration: TimeInterval, delayFactor: Double) -> Animation {
-		return { cell, indexPath, tableView in
-			cell.transform = CGAffineTransform(translationX: 0, y: rowHeight / 2)
-			cell.alpha = 0
+    static func makeMoveUpWithFade(rowHeight: CGFloat, duration: TimeInterval, delayFactor: Double) -> Animation {
+        return { cell, indexPath, scrollView in
+            cell.transform = CGAffineTransform(translationX: 0, y: rowHeight / 2)
+            cell.alpha = 0
 
-			UIView.animate(
-				withDuration: duration,
-				delay: delayFactor * Double(indexPath.row),
-				options: [.curveEaseInOut],
-				animations: {
-					cell.transform = CGAffineTransform(translationX: 0, y: 0)
-					cell.alpha = 1
-			})
-		}
-	}
+            UIView.animate(
+                withDuration: duration,
+                delay: delayFactor * Double(indexPath.row),
+                options: [.curveEaseInOut],
+                animations: {
+                    cell.transform = CGAffineTransform(translationX: 0, y: 0)
+                    cell.alpha = 1
+            })
+        }
+    }
 }
